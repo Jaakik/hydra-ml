ccf4a2d345055dc59b341d0d643b9e78732c0325
Jaakik <jaakik.marouane@epfl.ch>
2021-09-26 16:42:31 +0200

    mouse custom dataset

---
+++ src/datamodules/datasets/mouse_dataset.py
@@ -0,0 +1,18 @@
+from torch.utils.data import Dataset
+import numpy as np
+
+
+class MouseDataset(Dataset):
+    def __init__(self, data_path, ann_path=None):
+        self.ann = np.load(ann_path, allow_pickle=True).item()
+        self.data = np.load(data_path, allow_pickle=True).item()
+
+    def __len__(self):
+        return len(self.data.keys())
+
+    def __getitem__(self, idx):
+        seq_key = self.data.keys()[idx]
+        if self.ann_path is not None:
+            return self.data.values()[idx], seq_key, self.ann['sequences'][seq_key]['anntations']
+        else:
+            return self.data.values()[idx], seq_key
